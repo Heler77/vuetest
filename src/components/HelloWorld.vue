53a6a6f0d3efbcb67cf61defb091ad46dc970235
Heler Paez <heler2091@gmail.com>
2022-12-01 08:43:52 -0500

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,222 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <h1>Connect Wallet</h1>
+        <!-- <button @click="" >Connect</button> -->
+        <button class="button" @click="connectWallet" >Connect</button>
+        <br>
+        <br>
+        <span>Address:  {{ walletAddress }} </span>
+        <br>
+        <span>SubAddress:  {{ walletAddress.substring(0, 4) + '...' }} </span>
+        <br>
+        <span> {{ networkMessage }} </span>
+        <br>
+        <br>
+        <button class="button" @click="connectMetamaskMobile" >Connect on Mobile</button>
+        <br>
+        <br>
+        <span>BNB Balance:  {{ walletBalance / 1000000000000000000 }} </span>
+        <br>
+        <span> {{needWallet}} </span>
+        <span> BUSD Balance:  {{ BUSDBalance }} </span>
+        <!-- <button @click="takeAddress"> Obterner Direccion de Wallet  </button> -->
+        <!-- <span> Direccion de la wallet: {{ Address }}     </span> -->
+
+<!-- Renderizado Condicional -->
+
+
+    </div>
 </template>
 
 <script>
+
+// Imports
+//import connectWallet from '@/helpers/connectWallet.js'
+
+import Web3 from "web3/dist/web3.min"
+//import Web3 from 'web3' ==>> // Libreria incompatible con versiones de Webpack inferiores a la 5
+
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+
+    data() {
+        return {
+            walletAddress: "xxxxxxxxxxxxxxxxxxxxxx",
+            needWallet: "",
+            walletBalance: "",
+            connected: false,
+            networkMessage: "",
+            BUSDBalance: 0
+            // Address:  variable de la direccion de cartera
+            // coneccted = false
+        }
+    },
+
+    methods: {
+        connectWallet() {
+            //let web3Instance
+              const web3 = new Web3(window.ethereum)
+              const NODE_URL = 'https://node.philcoin.io/97';
+              const INTERFACE = require('../abi.json');
+
+              const getMetamaskProvider  = async() =>  {
+                  // check window ethereum provider
+                  if (window.ethereum) {
+                      try {
+                          await window.ethereum.request({ method: "eth_requestAccounts" }); //window.ethereum.enable()
+
+                          if(window.ethereum && window.ethereum.isMetamask ){alert(" alerta",typeof window.ethereum); alert("entrar en mobile"); }
+                          //web3Instance = web3
+                      } catch(error) {
+                          console.log(error)
+                      }
+                      web3.eth.getAccounts( async(err, accounts) => {
+                          //this.walletAddress;
+                          if (err != null)
+                          return console.log("NETWORK_ERROR")
+                          if (accounts.length === 0) {
+                          //this.walletAddress = "";
+                          console.log("NO_LOGIN");
+                          return;
+                          }
+                          this.walletAddress = await accounts[0];
+                          this.walletBalance = await web3.eth.getBalance(accounts[0])
+                          await getBUSDBalance(this.walletAddress)
+
+                      });
+                  }
+                  else {
+                  this.needWallet = "Please Install metamask"    //console.log('Please Install metamask')
+                  }
+              }
+
+              const getNode = () => {
+                  web3.eth.getNodeInfo(function(error, result){
+                      if(error){
+                      console.log( "error: " , error )
+                      }
+                      else{
+                      console.log( "result: ", result )
+                      }
+                  })
+              }
+            // Init
+                const chainRequire = async() => {
+                    let chainId = await web3.eth.getChainId();
+
+                        if (chainId == 56) {
+                            //return true
+                            console.log("This is the Binance MainNet")
+                            this.networkMessage = "This is the Binance MainNet"
+                        } else if (chainId == 97) {
+                            //return true
+                            console.log("This is the Binance TestNet")
+                            this.networkMessage = "This is the Binance TestNet"
+                        } else {
+                            //return false;
+                            console.log("This is not Binance Smart Chain")
+                            this.networkMessage = "This is not Binance Smart Chain"
+                        }
+                }
+            //end
+
+              const chainTest = async() => {
+              try {
+                await window.ethereum.request({
+                  method: 'wallet_switchEthereumChain',
+                  params: [{ chainId: '0x61' }],
+                });
+              } catch (switchError) {
+                // This error code indicates that the chain has not been added to MetaMask.
+                if (switchError.code === 4902) {
+                  try {
+                    await window.ethereum.request({
+                      method: 'wallet_addEthereumChain',
+                      params: [
+                        {
+                          chainId: '0x61',
+                          chainName: 'BSC Test',
+                          rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545/'] /* ... */,
+                        },
+                      ],
+                    });
+                  } catch (addError) {
+                    // handle "add" error
+                  }
+                }
+                // handle other "switch" errors
+              }
+            }
+
+          //const NODE_URL = 'https://node.philcoin.io/97';
+          //const INTERFACE = require('../abi.json');
+
+          const getBUSDBalance = async( accountAddress ) => {
+
+            //const Web3 = require('web3');
+
+                let web3NodeProvider = new Web3(new Web3.providers.HttpProvider(NODE_URL));
+
+                //const contractAddress = '0x801Ea81b4d49c18800FAD8190C665ADcDE33c4F7'; // -->  Philcoin Contract Test Address
+                const contractAddress = '0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee'; // --> BUSD Contract Test Address
+                //const accountAddress =  '0x603731Df392e41144437611C11dE93949f3Dd94c';
+
+                //async function getTokenBalance(accountAddress, contractAddress){}
+
+                    const contract = await new web3NodeProvider.eth.Contract(INTERFACE, contractAddress);
+
+                    const balance_wei = await contract.methods.balanceOf(accountAddress).call();
+
+                    this.BUSDBalance = parseFloat(web3NodeProvider.utils.fromWei(balance_wei, 'ether'));
+
+                /// Ejemplo
+
+                //getTokenBalance(accountAddress, contractAddress).then(console.log).catch(console.log);
+
+          }
+
+    console.log('Implementacion del isConnected = ', window.ethereum.isConnected())
+
+
+
+            getMetamaskProvider()
+            chainTest()
+            chainRequire()
+            //getBUSDBalance(this.walletAddress)
+            getNode()
+
+        },
+
+        connectMetamaskMobile() {
+      if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+            console.log("Estás usando un dispositivo móvil!!");
+            alert("Estás usando un dispositivo móvil!!")
+            const dappUrl = window.location.href.split("//")[1].split("/")[0];
+            const metamaskAppDeepLink = "https://metamask.app.link/dapp/" + dappUrl;
+            window.open(metamaskAppDeepLink, "_self");
+            alert("Fin del proceso")
+        } else {
+            console.log("No estás usando un móvil");
+            alert("No estás usando un móvil")
+        }
+    }
+
+    },
+
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.button {
+  background-color: #555555;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 18px;
 }
 </style>
